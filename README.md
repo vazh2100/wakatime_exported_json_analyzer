02e016124b8ef12c8e4249159d2357fcaf400429
vazh2100 <vazh2100@list.ru>
2024-12-21 19:10:06 +0300

    ## Added outputs for various categories - added output for languages - added output for operating systems - added output for machines - added output for editors ## Refactored: improved script structure and readability

--- README.md
+++ README.md
@@ -1,25 +1,81 @@
-### Wakatime Exported JSON File Analyzer
+### The Wakatime Exported JSON Analyzer
 
-The **Wakatime Exported JSON File Analyzer** is a tool designed to analyze exported JSON files from the **Export** section in the account settings of [Wakatime](https://wakatime.com/settings/account).
+is a tool designed to analyze exported JSON file from the **Export**
+section in the account settings of [Wakatime](https://wakatime.com/settings/account).
 
-#### Features
+### Features
 
 1. **Projects Overview**  
-   Displays a list of projects along with the time spent on each project.
+   Displays a sorted list of projects along with the time spent on each project.  
+   Also, displays a sorted list of languages, operating systems, machines and editors along with the time spent on each
+   entry.
 
-2. **Project Search**  
-   Supports three types of project searches:  
-   - **Case-insensitive search**: Matches strings in project names, ignoring case.  
-   - **Regular expression search**: Allows flexible string matching using regex patterns.  
-   - **Exact name search**: Matches the full name of the project exactly as it appears.
+2. **Three types of project searches**  
+   *Case-insensitive*: Matches strings in project names, ignoring case.  
+   *Regular expression*: Allows flexible string matching using regex patterns.  
+   *Exact name*: Matches the full name of the project exactly as it appears.
 
-3. **Date Range Filtering**  
-   Allows filtering results by a specified date range to focus on relevant periods.
+3. **Date Range Filtering**
 
-#### Usage
+### Usage
 
-1. Export the data from the **Export** section in your Wakatime account settings.  
-2. Import the JSON file into the analyzer.  
-3. Utilize search and filtering features to analyze your time-tracking data efficiently.
+```commandline
+All time and all projects:
 
-Forked from [yammyshep/wakahack](https://github.com/yammyshep/wakahack)
+python wakahack.py -f wakatime.json -a
+Total time: 2078h 27m
+Projects:
+    ***: 644h 29m
+    ***: 610h 56m
+    ***: 151h 35m
+    ...
+```
+
+```commandline
+Searched projects in the current year:
+
+python wakahack.py -f wakatime.json -s waka -ad 2024-01-01
+Total time: 2h 25m
+Projects:
+    wakatime_exported_json_analyzer: 2h 10m
+    kWaka: 0h 15m
+```
+
+```commandline
+Searched projects by regular expression in specific month:
+
+python wakahack.py -f wakatime.json -r .*json.* -ad 2024-12-01 -bd 2024-12-31 
+Total time: 1113h 21m
+Projects:
+    wakatime_exported_json_analyzer: 2h 10m
+    infinite_json_destroyer: 1111h 11m
+```
+
+```commandline
+A searched project by its exact name all time before this year:
+
+python wakahack.py -f wakatime.json -p "wakatime_exported_json_analyzer" -bd 2024-01-01 
+Total time: 100h 7m
+Projects:
+    wakatime_exported_json_analyzer: 100h 7m
+```
+
+```commandline
+Search all time and all projects and show only operating systems, machines and editors:
+
+python wakahack.py -f wakatime.json -a -show ome
+Total time: 2071h 46m
+Operating systems:
+    Linux: 2016h 0m
+    ...
+Machines:
+    zen: 1699h 42m
+    ...
+Editors:
+    Android Studio: 1968h 31m
+    IntelliJ IDEA: 65h 12m
+    PyCharm: 2h 10m
+    ...
+```
+
+Fork of [yammyshep/wakahack](https://github.com/yammyshep/wakahack)
--- wakahack.py
+++ wakahack.py
@@ -4,55 +4,7 @@ import json
 import re
 from collections import defaultdict
 
-
-def match_project(project_name):
-    if args.search is not None:
-        if args.search.lower() in project_name['name'].lower():
-            projects.append({'name': project_name['name'], 'time': project_name['grand_total']['total_seconds']})
-            return True
-        else:
-            return False
-
-    if args.project is not None:
-        if args.project == project_name['name']:
-            projects.append({'name': project_name['name'], 'time': project_name['grand_total']['total_seconds']})
-            return True
-        else:
-            return False
-
-    if args.regex is not None:
-        if re.match(args.regex, project_name['name']):
-            projects.append({'name': project_name['name'], 'time': project_name['grand_total']['total_seconds']})
-            return True
-        else:
-            return False
-
-    if args.all is not None:
-        projects.append({'name': project_name['name'], 'time': project_name['grand_total']['total_seconds']})
-        return True
-
-
-def match_date(date):
-    project_date = datetime.datetime.strptime(date['date'], '%Y-%m-%d')
-    if args.before is not None:
-        before_date = datetime.datetime.strptime(args.before, '%Y-%m-%d')
-        if before_date < project_date:
-            return False
-
-    if args.after is not None:
-        after_date = datetime.datetime.strptime(args.after, '%Y-%m-%d')
-        if after_date > project_date:
-            return False
-
-    return True
-
-
-def display_time(seconds):
-    """Returns time in hours and minutes format"""
-    hours, minutes = divmod(int(seconds / 60), 60)
-    return f"{hours}h {minutes}m"
-
-
+##----Parser----##
 parser = argparse.ArgumentParser(description='Count hours in a wakatime data dump')
 parser.add_argument('-f', '--file', metavar='file', type=open, required=True)
 search = parser.add_mutually_exclusive_group(required=True)
@@ -62,29 +14,124 @@ search.add_argument('-p', '--project', metavar='<name>')
 search.add_argument('-r', '--regex', metavar='<regex>')
 parser.add_argument('-bd', '--before', metavar='YYYY-MM-DD')
 parser.add_argument('-ad', '--after', metavar='YYYY-MM-DD')
+parser.add_argument("-show", "--show", metavar="plome", default="p",
+                    help="output options: p - projects, l - languages, o - os, m - machines e - editors, ")
 
+##----Variables----##
 args = parser.parse_args()
-
 input_data = json.load(args.file)
-
+showP = "p" in args.show
+showL = "l" in args.show
+showO = "o" in args.show
+showM = "m" in args.show
+showE = "e" in args.show
 total_time = 0.0
-projects = []
+projects_dict = defaultdict(int)
+languages_dict = defaultdict(int)
+operating_systems_dict = defaultdict(int)
+editors_dict = defaultdict(int)
+machines_dict = defaultdict(int)
 
-for day in input_data['days']:
-    if match_date(day):
-        for project in day['projects']:
-            if match_project(project):
-                total_time = total_time + project['grand_total']['total_seconds']
 
-c = defaultdict(int)
-for d in projects:
-    c[d['name']] += d['time']
+##----Functions----##
+def match_project(project_entry):
+    if args.search is not None:
+        return args.search.lower() in project_entry['name'].lower()
+    if args.project is not None:
+        return args.project == project_entry['name']
+    if args.regex is not None:
+        return re.match(args.regex, project_entry['name'])
+    if args.all is not None:
+        return True
 
-# Sorting projects by time in descending order
-projects = sorted([{'name': name, 'time': time} for name, time in c.items()], key=lambda x: x['time'], reverse=True)
 
-if len(projects) > 0:
-    print("Counted time from matching projects: " + display_time(total_time))
-    for p in projects:
-        print(f"    {p['name']}: {display_time(p['time'])}")
+def match_date(date):
+    project_date = datetime.datetime.strptime(date['date'], '%Y-%m-%d')
+    if args.before is not None:
+        before_date = datetime.datetime.strptime(args.before, '%Y-%m-%d')
+        if before_date < project_date:
+            return False
+    if args.after is not None:
+        after_date = datetime.datetime.strptime(args.after, '%Y-%m-%d')
+        if after_date > project_date:
+            return False
+    return True
+
+
+def add_project(project):
+    projects_dict[project['name']] += project['grand_total']['total_seconds']
+    global total_time
+    total_time += project['grand_total']['total_seconds']
+
+
+def add_language(language):
+    languages_dict[language['name']] += language['total_seconds']
+
+
+def add_os(os):
+    operating_systems_dict[os['name']] += os['total_seconds']
+
+
+def add_editor(editor):
+    editors_dict[editor['name']] += editor['total_seconds']
+
+
+def add_machine(machine):
+    machines_dict[machine['name']] += machine['total_seconds']
+
+
+def display_time(seconds):
+    """Returns time in hours and minutes format"""
+    hours, minutes = divmod(int(seconds / 60), 60)
+    return f"{hours}h {minutes}m"
+
+
+def print_summary(title: str, data: list[tuple[any, int]]):
+    if len(data) > 0:
+        print(title + ":")
+        for item in data:
+            print(f"    {item[0]}: {display_time(item[1])}")
+        print()
+
+
+##----Execution Section----##
+def main():
+    # Processing
+    for day in input_data['days']:
+        if match_date(day):
+            for project in day['projects']:
+                if match_project(project):
+                    add_project(project)
+                    if showL:
+                        for language in project['languages']:
+                            add_language(language)
+                    if showO:
+                        for os in project['operating_systems']:
+                            add_os(os)
+                    if showE:
+                        for editor in project['editors']:
+                            add_editor(editor)
+                    if showM:
+                        for machine in project['machines']:
+                            add_machine(machine)
+    # Printing
+    print("Total time: " + display_time(total_time))
+    if showP:
+        projects = sorted(projects_dict.items(), key=lambda x: x[1], reverse=True)
+        print_summary("Projects", projects)
+    if showL:
+        languages = sorted(languages_dict.items(), key=lambda x: x[1], reverse=True)
+        print_summary("Languages", languages)
+    if showO:
+        operating_systems = sorted(operating_systems_dict.items(), key=lambda x: x[1], reverse=True)
+        print_summary("Operating systems", operating_systems)
+    if showM:
+        machines = sorted(machines_dict.items(), key=lambda x: x[1], reverse=True)
+        print_summary("Machines", machines)
+    if showE:
+        editors = sorted(editors_dict.items(), key=lambda x: x[1], reverse=True)
+        print_summary("Editors", editors)
+
+
+main()
 exit()
